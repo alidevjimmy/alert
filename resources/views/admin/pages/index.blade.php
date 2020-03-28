958c47ebda8a00e20254f9300d4aeb0494e7606b
alidevjimmy <59659737+alidevjimmy@users.noreply.github.com>
2020-03-29 02:00:27 +0430

    add phone count to admin panel

--- resources/views/admin/pages/index.blade.php
+++ resources/views/admin/pages/index.blade.php
@@ -33,4 +33,23 @@
             </div>
         </div>
     </div>
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-success shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">کاربران اپلیکیشن
+                        </div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">
+                            {{ count(\Illuminate\Support\Facades\DB::table('users')
+                                ->select('phone')->groupBy('phone')->get()) }}
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-users fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
