2add8be3083c900353b2162342f0e2d26c02563c
alidevjimmy <59659737+alidevjimmy@users.noreply.github.com>
2020-03-31 16:29:31 +0430

    fix user versioning bug url

--- routes/api.php
+++ routes/api.php
@@ -14,15 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::group([
     'namespace' => 'Api\v1',
     'as' => 'v1.',
     'prefix' => 'v1'
 ], function () {
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+    });
     Route::post('/register', 'UserController@register');
     Route::post('/checkcode', 'UserController@checkCode');
     Route::get('/voices', 'VoiceController@all');
